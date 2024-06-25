3c7604cc7c0f0387fcfaf0a21487c2b90a4271a1
Yael Antonio Chavez Atanacio <230110032@itsoeh.edu.mx>
2024-06-25 10:53:06 -0600

    Ejercicio 12: seccion b corregida

--- codigo_base/caracas.py
+++ codigo_base/caracas.py
@@ -1,10 +1,14 @@
 def plot_hist(clases, freq_absoluta, mrks, labelx, labely, titulo):
     import matplotlib.pyplot as plt
 
-    plt.figure(figsize=(12, 6))  # Set the figure size
+     # Ajustar el ancho de las barras según el tamaño de los datos
+    if max(mrks) > 500:
+        bar_width = 30.0
+    else:
+        bar_width = 15.0
 
     plt.bar(mrks, freq_absoluta,
-           width=0.3, edgecolor="k",
+           width=bar_width, edgecolor="k",
            color=["#14BF48", "#33FFBE", "#333CFF", "#FF3349", "#F6FF33", "#33FFBE"])
     
     plt.xticks(mrks,  fontsize=12)
@@ -12,6 +16,4 @@ def plot_hist(clases, freq_absoluta, mrks, labelx, labely, titulo):
     plt.ylabel(labely, fontsize=15)  # Y-axis label
     plt.title(titulo, fontsize=20)  # Title
     plt.grid()  # Enable grid
-    plt.show()  # Display the plot
-    
-    return clases, freq_absoluta, mrks
+    plt.show()  # Display the plot    
