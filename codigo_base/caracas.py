8522feaa6e315b5de99671a4effcee3d77720472
Yael Antonio Chavez Atanacio <230110032@itsoeh.edu.mx>
2024-06-27 09:17:43 -0600

    Ejercicio 8: Se agrego la respuesta al inciso a: Tipos de datos continuos

--- codigo_base/caracas.py
+++ codigo_base/caracas.py
@@ -14,8 +14,8 @@ def plot_hist(clases, freq_absoluta, mrks, labelx, labely, titulo):
            color=["#14BF48", "#33FFBE", "#333CFF", "#FF3349", "#F6FF33", "#33FFBE"])
     
     plt.xticks(mrks,  fontsize=12)
-    plt.xlabel(labelx, fontsize=15)  # X-axis label
-    plt.ylabel(labely, fontsize=15)  # Y-axis label
-    plt.title(titulo, fontsize=20)  # Title
-    plt.grid()  # Enable grid
-    plt.show()  # Display the plot    
+    plt.xlabel(labelx, fontsize=15)  
+    plt.ylabel(labely, fontsize=15)  
+    plt.title(titulo, fontsize=20)  
+    plt.grid()  
+    plt.show()      
